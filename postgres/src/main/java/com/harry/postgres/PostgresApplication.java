a4c237926a362f97e5fb998d1aa43611209c6910
eccentricharry <harinathrao13@gmail.com>
2025-09-23 11:46:34 +0530

    Connectring to PostgreSQL

---
+++ postgres/src/main/java/com/harry/postgres/PostgresApplication.java
@@ -0,0 +1,31 @@
+package com.harry.postgres;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@Log
+public class PostgresApplication implements CommandLineRunner {
+
+    private final DataSource dataSource;
+
+    public PostgresApplication(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("DataSource: " + dataSource.toString());
+        final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+        restTemplate.execute("select 1");
+    }
+}
---
+++ postgres/src/test/java/com/harry/postgres/PostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harry.postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
