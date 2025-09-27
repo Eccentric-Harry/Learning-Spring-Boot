0c763921223b47c8d1f8365d22a3f716e96bdd12
eccentricharry <harinathrao13@gmail.com>
2025-09-27 14:05:51 +0530

    Initial Commit

---
+++ LiftSimulator/src/test/java/com/harry/LiftSimulator/LiftSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harry.LiftSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
