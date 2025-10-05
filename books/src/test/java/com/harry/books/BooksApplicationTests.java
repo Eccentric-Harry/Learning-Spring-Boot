b731efe1f3e33a85336a1ed166d42f60ec0f6c99
eccentricharry <harinathrao13@gmail.com>
2025-10-05 09:09:25 +0530

    Initial Commit

---
+++ books/src/test/java/com/harry/books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harry.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
